17071c88a497432a446bb87e039b2d3f26fe5ac0
Maxi Gimenez <gimenez.maxi@gmail.com>
2021-04-30 16:34:06 +0200

    Initial commit

---
+++ src/views/Home.vue
@@ -0,0 +1,11 @@
+<template>
+  <main>
+    <div class="px-4 py-6 sm:px-0">
+      <div
+        class="border-4 border-dashed border-gray-200 rounded-lg h-96 p-4 text-center text-gray-400"
+      >
+        Here goes your content. You can also go the About page.
+      </div>
+    </div>
+  </main>
+</template>
